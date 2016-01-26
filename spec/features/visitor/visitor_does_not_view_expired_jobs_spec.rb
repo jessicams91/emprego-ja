0c5f9c11ed5039d2ea4786a9e7fbafc1620935c6
Bruno e Elton <pair_locaweb4@campuscode.com.br>
2016-01-26 16:10:13 -0200

    Features expired jobs

---
+++ spec/features/visitor/visitor_does_not_view_expired_jobs_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Visitor does not view expired jobs' do
+  scenario 'successfully' do
+    travel_to 100.days.ago do
+      job = create_job
+    end
+
+    job = Job.first
+
+    visit root_path
+
+    expect(page).not_to have_content job.title
+  end
+
+  scenario "and sees a warning message at the job's page" do
+    travel_to 100.days.ago do
+      job = create_job
+    end
+
+    job = Job.first
+
+    visit job_path(job)
+
+    expect(page).to have_content "Vaga Expirada"
+
+  end
+
+end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -30,7 +30,38 @@ RSpec.describe Job, type: :model do
         expect(job).to be_recent
       end
     end
-
   end
+
+  describe "#expired?" do
+
+    context "created today" do
+      it "is not expired" do
+        job = create_job
+        expect(job).not_to be_expired
+      end
+    end
+
+    context "created 91 days ago" do
+      it "is expired" do
+        job = nil
+        travel_to 91.days.ago do
+          job = create_job
+        end
+        expect(job).to be_expired
+      end
+    end
+
+    context "created 89 days ago" do
+      it "is not expired" do
+        job = nil
+        travel_to 89.days.ago do
+          job = create_job
+        end
+        expect(job).not_to be_expired
+      end
+    end
+  end
+
+
 
 end
