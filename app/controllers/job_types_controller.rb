6413a4a759d147a0937ccacc8959695027b9d7ec
Carlos e Jessica <carlos.jessica@campuscode.com.br>
2016-02-16 15:16:21 -0200

    Views translated, part 1

--- app/controllers/job_types_controller.rb
+++ app/controllers/job_types_controller.rb
@@ -3,4 +3,13 @@ class JobTypesController < ApplicationController
   def show
     @job_type = JobType.find(params[:id])
   end
+
+  def new
+    @job_type = JobType.new
+  end
+
+  def create
+    @job_type = JobType.create(name: params[:job_type][:name])
+    respond_with @job_type
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :jobs, only: [:show, :new, :create, :edit, :update]
   resources :companies, only: [:new, :create, :show, :edit, :update]
   resources :categories, only: [:new, :create, :show]
-  resources :job_types, only: [:show]
+  resources :job_types, only: [:show, :new, :create]
 
   namespace :api, defaults: { format: 'json' } do
     resources :jobs, only: [:index, :show, :companies, :categories]
